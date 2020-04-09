343bdf158c77359a123db923b58226f70c1691bb
Joshua <joshua.richardson@myunidays.com>
2020-04-09 13:09:00 +0100

    Fixed some bugs

--- Chat/Chat/Hubs/ChatHub.cs
+++ Chat/Chat/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace Chat.Hubs
@@ -8,6 +9,19 @@ namespace Chat.Hubs
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            Console.WriteLine("Hello");
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"Joined the group: {groupName}");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", Context.ConnectionId, $"Left teh group: {groupName}");
         }
     }
 }
--- Chat/Chat/wwwroot/js/chat.js
+++ Chat/Chat/wwwroot/js/chat.js
@@ -4,16 +4,17 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMMessage", function (user, message) {
-    var msg = memssage.replace(/&/g, "&amp;").replace(/</g, "&lt").replace(/>/g, "&gt;");
-    var encodingMsg = user + " says " + msg;
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
-    document.getElementById("messageList".appendChild(li);
+    document.getElementById("messageList").appendChild(li);
 });
 
 connection.start().then(function () {
-    document.getElemenntById("sendButton").disabled = false;
+    document.getElementById("sendButton").disabled = false;
+    connection.invoke("AddToGroup", "my group");
 }).catch(function (err) {
     return console.error((err.toString()));
 });
@@ -21,7 +22,7 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
 
-    var message = document.getElementById("messageInput".value;
+    var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
